42fc21b4f4783a4253db8903ba4056b7ee8cf1a8
iLoveDisco <tuey@rose-hulman.edu>
2018-07-03 01:14:28 -0400

    Added a scroll feature

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ class App {
     constructor () {
         this.currentIndex = 0;
         this.pictures = document.getElementsByClassName("slides");
+        this.navButtons = document.getElementsByClassName("nav");
+        this.renderItems()
         //this.carousel()
     }
 
@@ -13,6 +15,24 @@ class App {
         if (this.currentIndex > this.pictures.length) {this.currentIndex = 1}    
         this.pictures[this.currentIndex - 1].style.display = "";
         setTimeout(this.carousel.bind(this), 7000); // Change image every 2 seconds
+    }
+
+    scrollIntoView (el_id) {
+        const element = document.querySelector(`#${el_id}`)
+        element.scrollIntoView({behavior: "smooth", block: "start"})
+    }
+
+    renderItems() {
+        for(let i = 0; i < this.navButtons.length; i++) {
+            if (this.navButtons[i].innerHTML == "Home") {
+                this.navButtons[i]
+                    .addEventListener('click', this.scrollIntoView.bind(this, "homeScroll_id"))
+            }
+            else if (this.navButtons[i].innerHTML == "Programs") {
+                this.navButtons[i]
+                    .addEventListener('click', this.scrollIntoView.bind(this, "programsScroll_id"))
+            }
+        }
     }
 }
 
